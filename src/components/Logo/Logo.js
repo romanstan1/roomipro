3f4295f31b84c0b78ed2e6459dd50b7763d5ccda
Roman Stankiewicz <roman.stan3@gmail.com>
2018-09-26 11:22:03 +0100

    design of log in page

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import './Logo.css'
+
+const Logo = () =>
+  <div className='Logo'>
+    <span></span>
+    <Link to='/'>roomipro</Link>
+  </div>
+
+export default Logo
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import Nav from './Nav/Nav'
+import Logo from './Logo/Logo'
 
-export {Nav}
+export {Nav, Logo}
