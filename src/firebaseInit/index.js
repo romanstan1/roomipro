3ab9b7ca48e33ebd5bfb9f36da8b25354d5a1445
Roman Stankiewicz <roman.stan3@gmail.com>
2018-10-15 14:47:20 +0100

    experimental tab syncronisation

--- src/firebaseInit/index.js
+++ src/firebaseInit/index.js
@@ -13,6 +13,8 @@ const {arrayRemove, arrayUnion} = firebase.firestore.FieldValue
 const settings = {timestampsInSnapshots: true}
 
 firestore.settings(settings)
-firestore.enablePersistence()
+firestore.enablePersistence({experimentalTabSynchronization:true}).then(() => {
+       console.log("Woohoo! Multi-Tab Persistence!");
+})
 
 export {auth, persistence, secondaryAuth, firestore, arrayUnion, arrayRemove}
